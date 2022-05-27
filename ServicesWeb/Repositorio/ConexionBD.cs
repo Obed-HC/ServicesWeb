5457166fffa3b9f2c9a61faccef7cc87ee0d0896
Obed-HC <83016467+Obed-HC@users.noreply.github.com>
2022-05-26 20:46:27 -0500

    conf

--- ServicesWeb/Repositorio/ConexionBD.cs
+++ ServicesWeb/Repositorio/ConexionBD.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace ServicesWeb.Repositorio
 {
-    public class ConexionBD
+    public class ConexionBD 
     {
         public static string rutaConexion = "Data Source=144.22.57.33;Initial Catalog=APP_COLLECTOR;User ID=Obed; Password=Huamani_123!";
     }
